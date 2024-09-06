37720155973e0c9601f2c77a8a53bae9ff82a39f
ryanjammes16 <157386256+ryanjammes16@users.noreply.github.com>
2024-09-06 12:11:38 -0400

    Update Q2PrintArray.java

--- src/main/java/Q2PrintArray.java
+++ src/main/java/Q2PrintArray.java
@@ -2,7 +2,7 @@ public class Q2PrintArray {
     private static int[] array = {1, 2, 3, 4, 5, 6, 7};
 
     public static void main(String[] args) {
-        printRecursion(array.length); // Start with the length of the array
+        printRecursion(array.length);
     }
 
     private static void printRecursion(int i) {
@@ -11,10 +11,7 @@ public class Q2PrintArray {
             return;
         }
 
-        // Recursive case: print the element at index (array.length - i)
-        System.out.println(array[array.length - i]);
-
-        // Recursively call the function with i - 1
         printRecursion(i - 1);
+        System.out.println(array[i-1]);
     }
 }
