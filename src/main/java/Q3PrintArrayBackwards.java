b41c08b0eb2ef4878d45f7a06125b9e53c98cfa2
ryanjammes16 <157386256+ryanjammes16@users.noreply.github.com>
2024-09-06 12:10:41 -0400

    Update Q3PrintArrayBackwards.java

--- src/main/java/Q3PrintArrayBackwards.java
+++ src/main/java/Q3PrintArrayBackwards.java
@@ -2,7 +2,7 @@ public class Q3PrintArrayBackwards {
     private static int[] array = {1, 2, 3, 4, 5, 6, 7};
 
     public static void main(String[] args) {
-        printRecursion(array.length); // Start with the length of the array
+        printRecursion(array.length);
     }
 
     private static void printRecursion(int i) {
@@ -11,10 +11,7 @@ public class Q3PrintArrayBackwards {
             return;
         }
 
-        // Recursive call: first go deeper into the recursion
-        printRecursion(i - 1);
-
-        // Print the current element after the recursive call
         System.out.println(array[i - 1]);
+        printRecursion(i - 1);
     }
 }
