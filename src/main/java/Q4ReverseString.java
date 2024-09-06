7445a50888d34ceca9005a5a3d372e00f0a9971e
ryanjammes16 <157386256+ryanjammes16@users.noreply.github.com>
2024-09-06 12:04:16 -0400

    Update Q4ReverseString.java

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -2,21 +2,10 @@
 
 public class Q4ReverseString {
 
-    public static void main(String[] args) {
-        String str = "Test String";
-        String reversedStr = reverse(str);
-        System.out.println(reversedStr);
-    }
-
-    // Recursive method to reverse the string
     public static String reverse(String s) {
-        // Base case: if the string has length 1 or is empty, return it
-        if (s.length() == 1) {
-            return s;
-        }
-        // Recursive case: reverse the rest of the string and append the first character
-        else {
-            return reverse(s.substring(1)) + s.charAt(0);
-        }
+        // TODO: Implement the recursive function to reverse the string
+        if (s.length() <= 1){
+            returns;}
+        return reverse(s.substring(beginIndex:1)) + s.charAt(index:0);
     }
 }
