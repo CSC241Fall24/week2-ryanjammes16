0c733373ab46d4ce0fbbb30c412df71e03dd9fa9
ryanjammes16 <157386256+ryanjammes16@users.noreply.github.com>
2024-09-06 12:18:20 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -24,8 +24,8 @@ public class Q1WeirdFunction {
         f[1] = 1;
         f[2] =2;
 
-        for (int i = 3; I <=n; i++){
-            f[i] = f[i-1] + 2* f[i-2] + 3 * f[i-3];
+        for (int i = 3; i <= n; i++) {
+            f[i] = f[i - 1] + 2 * f[i - 2] + 3 * f[i - 3];
         }
         return f[n];
     }
